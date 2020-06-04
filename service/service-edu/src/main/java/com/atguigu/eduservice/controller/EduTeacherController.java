b9f7baa08e77e23c674b57447bbeb95ba74016e0
caiwenglong <836472117@qq.com>
2020-06-04 21:52:04 +0800

    init project

---
+++ service/service-edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -0,0 +1,36 @@
+package com.atguigu.eduservice.controller;
+
+
+import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.service.EduTeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 讲师 前端控制器
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-06-04
+ */
+@RestController
+@RequestMapping("/eduservice/edu-teacher")
+public class EduTeacherController {
+
+    @Autowired
+    private EduTeacherService teacherService;
+
+    @GetMapping("findAll")
+    public List<EduTeacher> list(){
+
+        return teacherService.list(null);
+
+    }
+}
+
