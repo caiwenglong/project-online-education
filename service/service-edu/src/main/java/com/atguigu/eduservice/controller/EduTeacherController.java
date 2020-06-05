2c59f736d4089f63fbf9d6cfe08b4a4175abd01c
caiwenglong <836472117@qq.com>
2020-06-05 14:58:57 +0800

    添加swagger功能

--- service/service-edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ service/service-edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -3,6 +3,10 @@ package com.atguigu.eduservice.controller;
 
 import com.atguigu.eduservice.entity.EduTeacher;
 import com.atguigu.eduservice.service.EduTeacherService;
+import com.atguigu.servicebase.config.SwaggerConfig;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +24,7 @@ import java.util.List;
  * @author testjava
  * @since 2020-06-04
  */
+@Api(tags = { SwaggerConfig.TAG_1 })
 @RestController
 @RequestMapping("/eduservice/edu-teacher")
 public class EduTeacherController {
@@ -27,15 +32,17 @@ public class EduTeacherController {
     @Autowired
     private EduTeacherService teacherService;
 
+    @ApiOperation(value = "查找所有的讲师")
     @GetMapping("findAll")
     public List<EduTeacher> list(){
-
         return teacherService.list(null);
-
     }
 
+    @ApiOperation(value = "通过讲师ID查找讲师")
     @GetMapping("delete/{id}")
-    public boolean removeById(@PathVariable String id) {
+    public boolean removeById(
+            @ApiParam(name = "id", value = "传入参数ID", required = true)
+            @PathVariable String id) {
         return teacherService.removeById(id);
     }
 
